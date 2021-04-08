44806d2338b66e21802b097401e35854355c9476
unknown <daniellim977@gmail.com>
2021-04-07 23:38:25 -0700

    used hooks to set async arrays

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import {useState} from 'react';
 import { getMerchants, getTransactions, getUsers } from './utils/GraphQLData';
 
 const Content = styled.div`
@@ -6,9 +7,53 @@ const Content = styled.div`
   padding: 10px;
 `;
 
+
 function App() {
+
+  const [merchants, setMerchants] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  getMerchants().then((value) => {
+    setMerchants(value);
+  });
+
+  getTransactions().then((value) => {
+    setTransactions(value);
+  });
+
+  getUsers().then((value) => {
+    setUsers(value);
+  });
+
+  
+  const merch = merchants;  
+  const transac = transactions;
+  const user = users;
+
+  for(var i in merch){
+    console.log(merch[i]);
+    // if(merch[i].currency === 'CAD'){
+    //   console.log(merch[i])
+    // }
+  }
+
+  for(var i in transac){
+    console.log(transac[i]);
+  }
+
+  for(var i in user) {
+    console.log(user[i]);
+  }
+ 
+
   return (
-    <Content>App goes here!</Content>
+    // <Content>App goes here!</Content>
+    <div>
+      <p>
+        SOMETHING GOES HERE
+      </p>
+    </div>
   );
 }
 
